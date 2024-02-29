bdae521f90ef1cd6cb36eba2ea58397f9ccd3037
Hussein <husseinelwakeel765@gmail.com>
2024-02-29 04:07:01 +0200

    Initial commit

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20,),
+              Text("Expanded and Flexible",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 20,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      color: Colors.purple,
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Flexible(
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      color: Colors.purple,
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Flexible(
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: 30,
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  SizedBox(width: 20,),
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      color: Colors.orange,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(child: SizedBox(height: 30,)),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
